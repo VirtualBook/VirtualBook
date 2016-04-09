2970e5e0c164b22b003d54d578950ff724ce0b7f
Vu Nguyen <ng-vu@liti.ws>
2016-04-09 14:31:14 +0700

    Add Sea Temp

---
+++ Unity/Assets/Scripts/GraphTextures.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GraphTextures {
+
+    public Texture[] currentTextures;
+    public Texture[] landTextures;
+
+	public static void LoadAll()
+    {
+
+    }
+
+    public static void SetGraph(string name)
+    {
+
+    }
+}
