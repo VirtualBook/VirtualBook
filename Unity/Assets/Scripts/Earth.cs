bd0395eaadb56f2309a43760fd4f8811cec2e356
Vu Nguyen <ng-vu@liti.ws>
2016-04-09 13:40:25 +0700

    Add Unity with Earth

---
+++ Unity/Assets/Scripts/Earth.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Earth : MonoBehaviour {
+
+    public bool isRotate = false;
+    public float angle = 0;
+    public float angleV = 10;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        processInput();
+
+	    if (isRotate)
+        {
+            angle += angleV * Time.deltaTime;
+            Quaternion rotate = Quaternion.Euler(0, angle, 0);
+            transform.rotation = rotate;
+        }
+	}
+
+    void processInput()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            isRotate = !isRotate;
+        }
+    }    
+
+    void reset()
+    {
+
+    }
+}
---
+++ Unity/Assets/Scripts/SunLight.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SunLight : MonoBehaviour {
+
+    public const float pi = 3.1415F;
+
+    public bool isRotate = false;
+    public float angle = 0;
+    public float angleV = 2;
+    public float distance = 20;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("z"))
+        {
+            isRotate = !isRotate;
+        }
+
+        if (isRotate)
+        {
+            angle += angleV * Time.deltaTime;
+            float z = distance * Mathf.Cos(angle);
+            float x = distance * Mathf.Sin(angle);
+            transform.position = new Vector3(x, 0, z);
+        }
+    }
+}
