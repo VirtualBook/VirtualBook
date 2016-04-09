1684330c0c6025394b6ad961f2a9f2b1682d0891
Nghia Tran <vinhnghiatran@gmail.com>
2016-04-09 13:23:46 +0700

    layout SignUpScreen

--- iPad/Source Code/VirtualBook-iPad/VirtualBook-iPad/FeLoginViewController.m
+++ iPad/Source Code/VirtualBook-iPad/VirtualBook-iPad/FeLoginViewController.m
@@ -7,9 +7,39 @@
 //
 
 #import "FeLoginViewController.h"
+#import <EAIntroView/EAIntroPage.h>
+#import <EAIntroPage.h>
+#import <EAIntroView.h>
+#import "FeSignInView.h"
+#import "FeSignUpView.h"
+#import "AppDelegate.h"
 
-@interface FeLoginViewController ()
+typedef NS_ENUM(NSInteger, FeLoginViewControllerState) {
+    FeLoginViewControllerState_Main = 0,
+    FeLoginViewControllerState_SignUp,
+    FeLoginViewControllerState_SignIn
+};
 
+
+@interface FeLoginViewController () <EAIntroDelegate, FeSignUpViewDelegate, FeSignInViewDelegate>
+{
+    CGSize screenSize;
+    BOOL isShowkeyboard;
+}
+
+@property (strong, nonatomic) EAIntroView *introView;
+@property (assign, nonatomic) FeLoginViewControllerState currentState;
+
+// Xib
+@property (strong, nonatomic) FeSignUpView *signUpView;
+@property (strong, nonatomic) FeSignInView *signInView;
+
+// UIs
+@property (weak, nonatomic) IBOutlet UIImageView *logoImageView;
+@property (weak, nonatomic) IBOutlet UILabel *titleLbl;
+@property (weak, nonatomic) IBOutlet UIButton *registerBtn;
+@property (weak, nonatomic) IBOutlet UIButton *loginBtn;
+@property (weak, nonatomic) IBOutlet UIVisualEffectView *blurView;
 @end
 
 @implementation FeLoginViewController
